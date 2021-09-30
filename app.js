0e9b96dccadf85b39a4a7a858824ffac1f513a4b
Roy Luzuriaga <luzuriagaroy@gmail.com>
2021-09-30 10:58:19 +0800

    activity 1 -Js  methods

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({    
+    data() {
+        return {        
+            Name: 'Roy Luzuriaga',
+            Age: 20,
+            url: "http://vuejs.org/images/logo.png"
+        };        
+    },
+});
+app.mount('#assignment');
